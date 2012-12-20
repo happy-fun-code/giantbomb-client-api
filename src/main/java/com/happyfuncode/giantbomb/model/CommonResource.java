7cd870cca8431064b7fe65cc58be8edc79c24f79
Brandon Vulaj <brandon.vulaj@gmail.com>
2012-12-20 12:13:12 -0500

    Added a common model
    Added a model to hold data that is common between most GiantBomb
    resources, such as date added, date updated, etc.

---
+++ src/main/java/com/happyfuncode/giantbomb/model/CommonResource.java
@@ -0,0 +1,14 @@
+package com.happyfuncode.giantbomb.model;
+
+import java.util.Date;
+
+/**
+ * A model to hold data common between GiantBomb resources
+ * 
+ * @author Brandon Vulaj
+ * 
+ */
+public abstract class CommonResource {
+	private Date dateAdded;
+	private Date dateLastUpdated;
+}
